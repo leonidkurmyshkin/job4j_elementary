f0bf64cedbbffe6ada371566a3936d9b862b4442
leonidkurmyshkin <leonid2073@gmail.com>
2021-01-16 23:23:45 +0300

    9.2. Checkstyle [#446807]

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -2,9 +2,9 @@ package ru.job4j.condition;
 
 public class SqArea {
     public static double square(double p, double k) {
-        double rsl = k * Math.pow(p / (2 * (k + 1)), 2);
-        return rsl;
+        return k * Math.pow(p / (2 * (k + 1)), 2);
     }
+
     public static void main(String[] args) {
         double result1 = SqArea.square(4, 1);
         System.out.println(" p = 4, k = 1, s = " + result1);
