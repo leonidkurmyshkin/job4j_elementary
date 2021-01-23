2933b898c206b743a3cd5e27660d9e478b5522c6
leonidkurmyshkin <leonid2073@gmail.com>
2021-01-23 12:36:19 +0300

    5.5. Простое число [#446851]. Исправил.

--- src/ru/job4j/loop/Factorial.java
+++ src/ru/job4j/loop/Factorial.java
@@ -3,11 +3,9 @@ package ru.job4j.loop;
 public class Factorial {
     public static int calc(int n) {
         int result = 1;
-         if (n > 1) {
-             for (int index = 2; index <= n; index++) {
-                 result = result * index;
-             }
-         }
+            for (int index = 2; index <= n; index++) {
+                result = result * index;
+            }
          return result;
     }
 }
