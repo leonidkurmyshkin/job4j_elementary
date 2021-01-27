96e540866a80ee671ad708eadb2da24fce9004bb
leonidkurmyshkin <leonid2073@gmail.com>
2021-01-27 21:24:33 +0300

    6.6.2. Двухмерный массив. Циклы. [#446780]

---
+++ test/ru/job4j/array/SkipNegativeTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class SkipNegativeTest {
+
+    @Test
+    public void skip1() {
+        int[][] in = {
+                {1, -2},
+                {1, 2}
+        };
+        int[][] expect = {
+                {1, 0},
+                {1, 2}
+        };
+        int[][] rsl = SkipNegative.skip(in);
+        assertThat(rsl, is(expect));
+    }
+
+    @Test
+    public void skip2() {
+        int[][] in = {
+                {1, -2, -5},
+                {-1, 2, 4}
+        };
+        int[][] expect = {
+                {1, 0, 0},
+                {0, 2, 4}
+        };
+        int[][] rsl = SkipNegative.skip(in);
+        assertThat(rsl, is(expect));
+    }
+}
