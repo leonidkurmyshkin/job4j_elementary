ede857fcfad82b6e7f3d9194fb8141a101b0756a
leonidkurmyshkin <leonid2073@gmail.com>
2021-01-29 19:12:04 +0300

    6.9. Жадный алгоритм. Сдача в кофе машине [#446759]

---
+++ test/ru/job4j/sort/MachineTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.sort;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class MachineTest {
+    @Test
+    public void whenEquals() {
+        int[] expected = {};
+        int[] rsl = Machine.change(100, 100);
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void when50by35() {
+        int[] expected = {10, 5};
+        int[] rsl = Machine.change(50, 35);
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void when50by2() {
+        int[] expected = {10, 10, 10, 10, 5, 2, 1};
+        int[] rsl = Machine.change(50, 2);
+        assertThat(rsl, is(expected));
+    }
+}
