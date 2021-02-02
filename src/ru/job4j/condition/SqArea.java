5f3623a40f99e454e47643154cbe5b18c29ac2d2
leonidkurmyshkin <leonid2073@gmail.com>
2021-02-02 23:20:09 +0300

    1. Чтение кода и базовые ошибки [#446930]

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -6,9 +6,9 @@ public class SqArea {
     }
 
     public static void main(String[] args) {
-        double result1 = SqArea.square(4, 1);
-        System.out.println(" p = 4, k = 1, s = " + result1);
-        result1 = SqArea.square(6, 2);
-        System.out.println(" p = 6, k = 2, s = " + result1);
+        double result = SqArea.square(4, 1);
+        System.out.println(" p = 4, k = 1, s = " + result);
+        result = SqArea.square(6, 2);
+        System.out.println(" p = 6, k = 2, s = " + result);
     }
 }
